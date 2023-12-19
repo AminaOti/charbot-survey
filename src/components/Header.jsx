3e8c799c0a92ffe420d63317b99666325896f733
Amina Oti <aminaoti@aminas-mbp.home>
2023-12-19 16:01:37 +0000

    feat: first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header({title}){
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+// // export default function Header({title}){
+
+
+// return( 
+//     <header className="header">
+//         <img src={logoImg} alt="header-logo"></img>
+//         <h1>{title} </h1>
+//     </header>
+  
+//    )
+// }

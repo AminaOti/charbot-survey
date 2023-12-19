7857dc434f18c0dcd800f93c04c0d9eb2c27d49c
Amina Oti <aminaoti@aminas-mbp.home>
2023-12-19 16:17:53 +0000

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import './App.css'
 import Header from './components/Header';
 
+
+
 function App() {
 
   return (
-    <>
+    <bodgy>
+      <image src="surveyLogo"></image>
      <Header title="Survey"></Header>
-    </>
+
+    </bodgy>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,14 @@
+
+import logoImg from '../assets/quiz-logo.png';
+
 export default function Header({title}){
-    return (
-        <header className="header">
-            <h1>{title}</h1>
-        </header>
-    )
+
+
+   return( 
+    <header className="header">
+        <img src={logoImg} alt="header-logo"></img>
+        <h1>{title} </h1>
+    </header>
+    
+   )
 }
-
-// // export default function Header({title}){
-
-
-// return( 
-//     <header className="header">
-//         <img src={logoImg} alt="header-logo"></img>
-//         <h1>{title} </h1>
-//     </header>
-  
-//    )
-// }

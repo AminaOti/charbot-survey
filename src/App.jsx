24f4c886d805ed799451cd0dc73ee245d8ef8b3e
Amina Oti <aminaoti@Aminas-MacBook-Pro.local>
2023-12-21 11:48:02 +0000

    feat: split first and second question

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 // import './App.css'
+import {useState} from 'react'
 import Header from './components/Header';
 import RatingScaleQuestion from './components/RatingScaleQuestion';
 import OpenEndedQuestion from './components/OpenEndedQuestion';
 
+const NUMBER_OF_QUESTIONS = 2;
+const FIRST_QUESTION_NUMBER = 1;
 
 
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(FIRST_QUESTION_NUMBER);
+  let quizQuestion = (<RatingScaleQuestion></RatingScaleQuestion>)
+
+
+  function handleNext(){
+    let nextQuestion = currentQuestion +1;
+    if(nextQuestion>2)(
+      nextQuestion = 2
+    )
+    setCurrentQuestion(nextQuestion);
+  }
+
+  if(currentQuestion!=FIRST_QUESTION_NUMBER)
+  {
+    quizQuestion= (<OpenEndedQuestion></OpenEndedQuestion>)
+  }
+
 
   return (
     <body>
      <Header title="Survey"></Header>
         <div id="survey-questions">
-          <RatingScaleQuestion ></RatingScaleQuestion>
-          <OpenEndedQuestion></OpenEndedQuestion>
+          <p>Question {currentQuestion}/{NUMBER_OF_QUESTIONS}</p>
+          {quizQuestion}
+          <button onClick={handleNext}> Next </button>
         </div>
     </body>
   )
